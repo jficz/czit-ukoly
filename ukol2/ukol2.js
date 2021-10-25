bd5dd71834c489011f438b4ab0a69c4b14c50ab6
Jakub Fišer <jakub@ufiseru.cz>
2021-10-25 20:32:36 +0200

    Opravena chybna podminka v ukolu c. 2

--- ukol2/ukol2.js
+++ ukol2/ukol2.js
@@ -1,9 +1,9 @@
 function max(a,b) {
-    return a < b ? a : b
+    return a > b ? a : b
 }
 
-console.log(1,2)
-console.log(-5, -2)
-console.log(16,22)
-console.log(21,20)
-console.log(0,0)
+console.log(max(1,2))
+console.log(max(-5, -2))
+console.log(max(16,22))
+console.log(max(21,20))
+console.log(max(0,0))
